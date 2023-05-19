28e52816ddda7bb466a99aab5098e8026efdaf6f
Максим <101255482+kaemey@users.noreply.github.com>
2023-05-19 18:23:59 +0300

    Add files via upload

---
+++ Проект VBA 2008 Clicker#/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿Option Explicit On
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'TextBox2
+        '
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'NotifyIcon1
+        '
+        resources.ApplyResources(Me.NotifyIcon1, "NotifyIcon1")
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'CheckBox1
+        '
+        resources.ApplyResources(Me.CheckBox1, "CheckBox1")
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.IsMdiContainer = True
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+    Dim But As String
+    Dim turn As Boolean = False
+
+    Private Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
+    Public Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Long) As Integer
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Long, ByVal dwExtraInfo As Long)
+    Private Const VK_CAPITAL = &H14
+    Const KEYEVENT_up = &H2
+    Private Const MOUSEEVENTF_RIGHTDOWN = &H8  'Нажать правую кнопку
+    Private Const MOUSEEVENTF_RIGHTUP = &H10  'Отпустить правую кнопку
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+                           Handles MyBase.Load
+
+        NotifyIcon1.BalloonTipText = "Clicker# позволяет кликать на определённую клавишу постоянно"
+        NotifyIcon1.Text = "Clicker#"
+        NotifyIcon1.ShowBalloonTip(5000)
+        CreateContextMenu()
+
+    End Sub
+
+    Public Sub CreateContextMenu()
+
+        'Define New Context Menu and Menu Item 
+        Dim contextMenu As New ContextMenu
+        Dim menuItem As New MenuItem("Закрыть")
+        contextMenu.MenuItems.Add(menuItem)
+
+        ' Associate context menu with Notify Icon 
+        NotifyIcon1.ContextMenu = contextMenu
+
+        'Add functionality for menu Item click 
+        AddHandler menuItem.Click, AddressOf menuItem_Click
+
+    End Sub
+
+    Private Sub menuItem_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.WindowState = FormWindowState.Minimized
+        Me.Hide()
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub Form1_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Timer1.Enabled = Not Timer1.Enabled
+
+        If Timer1.Enabled Then
+            Button2.Text = "Отключить"
+            turn = False
+        Else
+            Button2.Text = "Запустить"
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        But = Val(TextBox1.Text)
+
+        Static NtKl(But) As Boolean
+        'Можно также опрашивать в цыкле
+
+        If GetAsyncKeyState(But) <> 0 Then
+            If NtKl(But) = False Then
+                turn = Not turn
+            End If
+            NtKl(But) = True
+        Else
+            NtKl(But) = False
+        End If
+
+        If turn Then
+            If CheckBox1.Checked Then
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+            Else
+                Call keybd_event(Val(TextBox2.Text), 0, 0, 0)
+                Call keybd_event(Val(TextBox2.Text), 0, KEYEVENT_up, 0)
+            End If
+        End If
+
+    End Sub
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
